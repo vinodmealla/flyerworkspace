7d69abb761be9e7de260efbe8d48dff33c2f54d0
vinod mealla <vinodkumarmealla@gmail.com>
2017-10-04 15:01:27 +0530

    dx
    cc

---
+++ com.gvm.sample1/src/main/java/com/gvm/org/com/gvm/sample1/App.java
@@ -0,0 +1,13 @@
+package com.gvm.org.com.gvm.sample1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
